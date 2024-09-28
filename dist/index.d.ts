4ca6c7861a273a43c85a34d2e0507e21e8b11cec
Aleksandr Beshkenadze <beshkenadze@gmail.com>
2024-09-28 21:22:55 +0300

    Refactor package.json and update project metadata

---
+++ dist/index.d.ts
@@ -0,0 +1,3 @@
+export declare function parseHumanReadableTime(input: string): number;
+export declare function parseHumanReadableSize(input: string): number;
+export declare function run(): Promise<void>;
